3d5d8720d5498cf72a7b609b3b39d3bab9169b0b
archeramores <79795086+archeramores@users.noreply.github.com>
2021-11-17 19:09:09 +0800

    commit array

--- dashboard/festDash.php
+++ dashboard/festDash.php
@@ -583,7 +583,7 @@ h3{
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[1][3];
+      td = tr[i].getElementsByTagName("td")[1, 3];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
